ffc393ad7290d982c5a31a12ede61f03dfc42d53
mkitani <dynamicderby@gmail.com>
2017-02-06 00:23:38 +0900

    20170205Lesson

--- app/controllers/entform_usrmail_controller.rb
+++ app/controllers/entform_usrmail_controller.rb
@@ -2,5 +2,12 @@ class EntformUsrmailController < ApplicationController
   def ctrl_entform_usrmail
   end
   def ctrl_entform_usrmail_done
+  	user_params
+  	puts params[:email]
+  	puts params[:password]
   end
+  private
+  	def user_params
+  		params.require(:user).permit(:email,:password)
+  	end
 end
