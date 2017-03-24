663ebf0b800cad86856514c58239558a43305558
mkitani <dynamicderby@gmail.com>
2017-03-24 23:00:12 +0900

    Follow REST API(resources) and Create user_profile model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,17 @@ class UsersController < ApplicationController
   def new
   end
   def create
-  	user_params
+#  	user_params
+  	params = user_params
   	puts params[:email]
   	puts params[:password]
+
+  	puts "testtest"
+#  	binding.pry
+    user = User.new
+    user.email = params[:email]
+    user.password = params[:password]
+    user.save
   end
   private
   	def user_params
